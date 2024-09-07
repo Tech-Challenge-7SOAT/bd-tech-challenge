3d569ec0e5d54c2e0cba9bf6465727a72c792a8c
caio367 <caio.antuns@gmail.com>
2024-09-07 10:51:15 -0300

    Adding db variables and updating rds configuration

--- infra/main.tf
+++ infra/main.tf
@@ -3,22 +3,23 @@ provider "aws" {
 }
 
 resource "aws_db_instance" "default" {
-  allocated_storage    = 20
+  allocated_storage    = 5
   storage_type         = "gp2"
-  engine               = "mysql"
-  engine_version       = "5.7"
+  engine               = "postgres"
+  engine_version       = "13"
   instance_class       = "db.t2.micro"
-  name                 = "mydb"
-  username             = "foo"
-  password             = "foobarbaz"
-  parameter_group_name = "default.mysql5.7"
+  db_name              = var.DB_NAME
+  username             = var.DB_USERNAME
+  password             = var.DB_PASSWORD
+  parameter_group_name = "default.postgres13"
   skip_final_snapshot  = true
+  publicly_accessible  = false
 }
 
 resource "null_resource" "db_migrations" {
   depends_on = [aws_db_instance.default]
 
   provisioner "local-exec" {
-    command = "mysql -h ${aws_db_instance.default.address} -u ${var.db_username} -p${var.db_password} < db_schema.sql"
+    command = "PGPASSWORD=${var.DB_PASSWORD} psql -h ${aws_db_instance.default.address} -U ${var.DB_USERNAME} -d ${var.DB_NAME} -f db_schema.sql"
   }
 }
---
+++ infra/variables.tf
@@ -0,0 +1,11 @@
+variable "DB_NAME" {
+  description = "The name of the database"
+}
+
+variable "DB_USERNAME" {
+    description = "The username for the database"
+}
+
+variable "DB_PASSWORD" {
+    description = "The password for the database"
+}
