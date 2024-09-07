3f424f567f4ac2c176dbdf1c5fce473929be380f
caio367 <caio.antuns@gmail.com>
2024-09-07 12:20:55 -0300

    Updating provider

--- infra/variables.tf
+++ infra/variables.tf
@@ -9,3 +9,15 @@ variable "DB_USERNAME" {
 variable "DB_PASSWORD" {
     description = "The password for the database"
 }
+
+variable "AWS_ACCESS_KEY_ID" {
+  description = "The AWS access key"
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "The AWS secret access"
+}
+
+variable "AWS_SESSION_TOKEN" {
+  description = "The AWS session token"
+}
