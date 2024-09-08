f43ebc7e4bd3674b2e0f4268c3a3a72210371e96
caio367 <caio.antuns@gmail.com>
2024-09-08 01:18:54 -0300

    Updating terraform

--- infra/main.tf
+++ infra/main.tf
@@ -2,7 +2,7 @@ resource "aws_db_instance" "default" {
   allocated_storage    = 5
   storage_type         = "gp2"
   engine               = "postgres"
-  engine_version       = "11.5"
+  engine_version       = "13.1"
   instance_class       = "db.t3.micro"
   db_name              = var.DB_NAME
   username             = var.DB_USERNAME
