bcc0c1a2a9680a4301d5cef42f5ff99510a2d75a
caio367 <caio.antuns@gmail.com>
2024-09-09 02:43:59 -0300

    Updating terraform  with network configurations

--- infra/provider.tf
+++ infra/provider.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  region = var.AWS_REGION
+  region = var.aws_region
   access_key  = var.AWS_ACCESS_KEY_ID
   secret_key  = var.AWS_SECRET_ACCESS_KEY
   token       = var.AWS_SESSION_TOKEN
