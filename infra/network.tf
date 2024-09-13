40fe072198e2d073f2e501591030c45e143ef789
caio367 <caio.antuns@gmail.com>
2024-09-13 02:09:37 -0300

    Updating network configuration - enable DNS

--- infra/network.tf
+++ infra/network.tf
@@ -37,6 +37,9 @@ resource "aws_db_subnet_group" "fastfood_db_subnet_gp" {
 resource "aws_vpc" "fastfood_vpc" {
   cidr_block = var.vpc_cidr_block
 
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
     tags = {
         Name = "fastfood_vpc"
     }
